ce85caef6867741ad23e0551a665c67cecd757bf
epord <pedro.balaguer@hotmail.com>
2019-03-20 01:19:06 -0300

    Add cool solution printer

---
+++ p5/solutionPrinter.js
@@ -0,0 +1,69 @@
+const windowSize = 800; // px
+const fps = 3;
+const states = [];
+let boardSize;
+let file;
+
+function preload() {
+    file = loadStrings('solution.out');
+}
+
+function setup() {
+    createCanvas(windowSize, windowSize);
+    noStroke();
+    frameRate(fps);
+    console.log(file)
+    boardSize = parseInt(file[0].trim());
+    file.filter((content, line) => content && line !== 0)
+        .forEach((content, line) => {
+            if ((line % 4) === 0) {
+                states.push([])
+            }
+            states[Math.floor(line/boardSize)].push(content);
+        });
+    console.log(states)
+}
+
+const drawCell = (row, col, color, number = '') => {
+    const cellDiameter = windowSize/boardSize;
+    const x = cellDiameter * (row + .5);
+    const y = cellDiameter * (col + .5);
+    fill(color);
+    circle(y, x, cellDiameter * .9 / 2);
+    if (number){
+        const tSize = cellDiameter * 0.5;
+        fill(255);
+        textAlign(CENTER);
+        textSize(tSize);
+        text(number, y, x + tSize/4);
+    }
+};
+
+const renderCell = (row, col, value) => {
+    if (/^\d*$/.test(value)) {
+        return drawCell(row, col, color(37, 178, 221), value);
+    }
+    switch (value) {
+        case '.':
+            drawCell(row, col, color(234));
+            break;
+        case 'X':
+            drawCell(row, col, color(249, 31, 55));
+            break;
+        case 'O':
+            drawCell(row, col, color(37, 178, 221));
+            break;
+    }
+};
+
+let currentBoardIdx = 0;
+function draw () {
+    background(250);
+    const currentBoard = states[(currentBoardIdx++) % states.length];
+    currentBoard.forEach((line, i) => {
+        const row = line.trim().split(' ');
+        row.forEach((cell, j) => {
+            renderCell(i, j, cell);
+        })
+    });
+}
--- src/ar/edu/itba/sia/Ohn0/Main.java
+++ src/ar/edu/itba/sia/Ohn0/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileManager fm = new FileManager();
         State state = fm.readStateFromFile(Paths.get("board"));
-        System.out.println(state.getRepresentation());
+        System.out.println(state.getRepresentation() + state.getRepresentation());
     }
 
 }
