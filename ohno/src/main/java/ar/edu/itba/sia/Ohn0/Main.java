8211db04064bb9e2bc2465b11289b6ae38c07685
Juan Li Puma <juanlipuma94@gmail.com>
2019-03-24 18:17:34 -0300

    Extract repeated code in rule generator

--- ohno/src/main/java/ar/edu/itba/sia/Ohn0/Main.java
+++ ohno/src/main/java/ar/edu/itba/sia/Ohn0/Main.java
@@ -67,35 +67,33 @@ public class Main {
 
         for(i = 0; i < size; i++) {
             for(j = 0; j < size; j++) {
-                final int I = i, J = j;
-                rules.add(new Ohn0Rule("blue:" + i +"," + j, state -> {
-                    Board currentBoard = (Board) state;
-                    Board newBoard;
-                    if (!currentBoard.getCell(I, J).isBlank()) return Optional.empty();
-                    newBoard = currentBoard.switchColor(I, J, Color.BLUE);
-                    if(newBoard == null || !newBoard.isChangeValid(I,J)) {
-                        return Optional.empty();
-                    }
-                    else {
-                        return Optional.of(newBoard);
-                    }
-                }));
-
-                rules.add(new Ohn0Rule("red:" + i + "," + j, state -> {
-                    Board currentBoard = (Board) state;
-                    Board newBoard;
-                    if (!currentBoard.getCell(I, J).isBlank()) return Optional.empty();
-                    newBoard = currentBoard.switchColor(I, J, Color.RED);
-                    if(newBoard == null || !newBoard.isChangeValid(I,J)) {
-                        return Optional.empty();
-                    }
-                    else {
-                        return Optional.of(newBoard);
-                    }
-                }));
+                rules.add(generateRule(i, j, Color.BLUE));
+                rules.add(generateRule(i, j, Color.RED));
             }
         }
 
         return rules;
+    }
+
+    /**
+     * Generate an {@link Ohn0Rule} that fills the cell at (i, j) with the given color if the cell is not blank.
+     *
+     * @param i     X coordinate
+     * @param j     Y coordinate
+     * @param color Color to fill if not empty
+     * @return The corresponding rule
+     */
+    private static Ohn0Rule generateRule(final int i, final int j, Color color) {
+        return new Ohn0Rule(String.format("%s @ (%d,%d)", color.name(), i, j), state -> {
+            Board currentBoard = (Board) state;
+            if (!currentBoard.getCell(i, j).isBlank()) return Optional.empty();
+            Board newBoard = currentBoard.switchColor(i, j, color);
+            if (newBoard == null || !newBoard.isChangeValid(i,j)) {
+                return Optional.empty();
+            }
+            else {
+                return Optional.of(newBoard);
+            }
+        });
     }
 }
