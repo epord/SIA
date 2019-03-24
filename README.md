cb9e200beb1ad4e7851744e8c797624c118e3228
Juan Li Puma <juanlipuma94@gmail.com>
2019-03-24 14:58:29 -0300

    Single jar (#1)
    Make project a multi-module Maven project, configure `mvn package` to create single jar with dependencies, add README with instructions

---
+++ README.md
@@ -0,0 +1,8 @@
+# SIA TP 1
+
+## Building
+1. `mvn package`
+1. Output file is under `target/gps-1.0.jar`, relative to projet root
+
+## Running
+1. `java -jar ohno/target/gps-1.0.jar`
---
+++ gps/src/test/java/ar/edu/itba/sia/GpsTest.java
@@ -0,0 +1,19 @@
+package ar.edu.itba.sia;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class GpsTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
