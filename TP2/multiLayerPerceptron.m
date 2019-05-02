9f83e280ff509f5cce77090160eb53e1c36205aa
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-02 16:03:53 -0300

    exp function added, but neural network doesnot learn

--- TP2/multiLayerPerceptron.m
+++ TP2/multiLayerPerceptron.m
@@ -43,13 +43,15 @@ function newVector = shuffle(vector, vectorSize)
 		endfor
 endfunction
 
-function result = g(input) 
-	result = tanh(input);
+function [result] = g(input) 
+	#result = tanh(input);
+	result = 1.0 ./ (1.0 + exp(-input));
 endfunction
 
 #Where output is tanh(input)
-function result = gPrima(output)
-	result = 1 - output.** 2;
+function [result] = gPrima(output)
+	#result = 1 - output.** 2;
+	result = output .* (1 - output);
 endfunction
 
 ####################################################################################################
@@ -62,7 +64,7 @@ outputUnits  	= 1;
 inputUnits   	= 2;
 currentError 	= 1;
 maxError		= 0.005;
-learningFactor  = 0.4;
+learningFactor  = 0.1;
 epoch			= 1;
 acumError		= 0;
 
@@ -72,7 +74,7 @@ Weights 		= cell(hiddenLayers + 1, 1);
 Weights(1)		= (rand(hiddenUnits, inputUnits + 1) - 0.5);
 Weights(2)		= (rand(outputUnits, hiddenUnits + 1) - 0.5);
 
-ExpectedOutputs = [-1, 1, 1, -1]; #hardcoded for inputUnits = 2
+ExpectedOutputs = [0, 1, 1, 0]; #hardcoded for inputUnits = 2
 
 Outputs 		= cell(hiddenLayers + 1, 1);
 MembranePotentials = cell(hiddenLayers + 1, 1);
@@ -80,74 +82,70 @@ Deltas 			= cell(hiddenLayers + 1, 1);
 
 inputSize		= 2 ** inputUnits;
 Patterns 		= getPatterns(inputUnits);
-# Output 			= 0;
+analizedPatterns = 0;
 
 do 	
+	epoch
 	inputOrder = shuffle(1 : inputSize, inputSize);
 	
 	for index = 1 : inputSize
 		CurrentPattern  = Patterns(:, inputOrder(index));
-		Input 			= CurrentPattern 
+		Input 			= CurrentPattern #comment
 		
 		for currentLayer = 1 : hiddenLayers + 1
 			Output = cell2mat(Weights(currentLayer)) * Input;
 			MembranePotentials(currentLayer) = Output;
-			currentLayer
 			Output = g(Output);
-			if(currentLayer == hiddenLayers + 1)
-				Output = sign(Output);
-			endif
-			Output
 			Outputs(currentLayer) = Output;
-
 			
 			if(currentLayer <= hiddenLayers)
-				Input  = [-1; Output]
+				Input  = [-1; Output] #comment
 			endif
 		endfor 
 	
-		#if output differs
-		#fix weights and backpropagation
-		ExpectedOutput = ExpectedOutputs(inputOrder(index)); #TODO
-		ExpectedOutput #comment
-		currOutput = cell2mat(Outputs(hiddenLayers + 1))
+		ExpectedOutput = ExpectedOutputs(inputOrder(index)) #TODO
+		currOutput 	   = cell2mat(Outputs(hiddenLayers + 1))
 		
 		if(ExpectedOutput != currOutput)
-			acumError = (ExpectedOutput - cell2mat(Outputs(hiddenLayers + 1))) ** 2
-
+			acumError = acumError + (ExpectedOutput - cell2mat(Outputs(hiddenLayers + 1))) ** 2;#comment
 
 			#calculateDeltas
-			Deltas(hiddenLayers + 1) = gPrima(cell2mat(MembranePotentials(hiddenLayers + 1))) .* (ExpectedOutput - cell2mat(Outputs(hiddenLayers + 1)));
+			Deltas(hiddenLayers + 1) = gPrima(cell2mat(MembranePotentials(hiddenLayers + 1))) .* (ExpectedOutput - currOutput);
 			
 			for currentLayer = hiddenLayers : -1 : 1
-				currentWeights = cell2mat(Weights(currentLayer + 1)); #get matrix
+				currentWeights	       = cell2mat(Weights(currentLayer + 1)); #get matrix
 				currentWeights(:, [1]) = []; #remove bias weight
-				currentWeights = currentWeights'
-				Deltas(currentLayer) = gPrima(cell2mat(MembranePotentials(currentLayer))).* (currentWeights * cell2mat(Deltas(currentLayer + 1)))
+				currentWeights 		   = currentWeights';#comment
+				Deltas(currentLayer)   = gPrima(cell2mat(MembranePotentials(currentLayer))).* (currentWeights * cell2mat(Deltas(currentLayer + 1)));#comment
 			endfor
+			printf("\n\n");
+			Weights
+			printf("\n\n");
+			printf("\n\n");
+			Deltas
+			printf("\n\n");
 		
 			#update weights 
 			for currentLayer = 1 : hiddenLayers + 1
-				currentLayer
 				if(currentLayer == 1)
-					DeltaWeights = learningFactor * cell2mat(Deltas(currentLayer)) * (CurrentPattern')
+					DeltaWeights = learningFactor * cell2mat(Deltas(currentLayer)) * (CurrentPattern');#comment
 				else
-					#here problems
 					OutputWithBias = cell2mat(Outputs(currentLayer - 1))'; #get matrix and transpose
 					OutputWithBias = [-1, OutputWithBias]; #Add bias 
-					DeltaWeights = learningFactor * cell2mat(Deltas(currentLayer)) * OutputWithBias;
+					DeltaWeights = learningFactor * cell2mat(Deltas(currentLayer)) * OutputWithBias;#comment
 				endif
 				Weights(currentLayer) = cell2mat(Weights(currentLayer)) + DeltaWeights;
 			endfor
-			
-			epoch = epoch + 1;
-			if(mod(epoch, inputSize / 2) == 0)
-				currentError = acumError / 2;
+			analizedPatterns = analizedPatterns + 1;
+			if(mod(index, 4) == 0)
+				currentError = acumError / 2#comment
 				acumError = 0;
 			endif  
 		endif
 	endfor
-until (currentError < maxError)
+	epoch = epoch + 1;
+
+until (currentError < maxError  || epoch > 1000)
 
 
 
