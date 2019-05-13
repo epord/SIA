e7f584c298de56ea3b58bbaf40c5ed47683b4bd6
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-13 05:01:14 -0300

    script updated

--- TP2/TerrainNeuralNetwork/weightInitMethodFinder.sh
+++ TP2/TerrainNeuralNetwork/weightInitMethodFinder.sh
@@ -9,17 +9,19 @@ cp architecture.conf architecture.conf.bak
 rm configurationResults.txt > /dev/null 2>&1
 for quantity in $(seq 1 1 5)
   do
-		echo "Testing with $alphaMomentum nodes in the hidden layer"
+		echo "Testing iteration $quantity"
 		sleep 1
 		currentSeed=$RANDOM
 		sed -i "" "/.*rand(\"seed\",.*/s//rand(\"seed\", $currentSeed);/g" architecture.conf
 		
 		#random weights
+		echo -e "Testing random"
 		sed -i "" "/.*weightInitMethod.*/s//weightInitMethod = 0;/g" architecture.conf
 		octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt
 		#cat architecture.conf
 		
 		#fan-in
+		echo -e "Testing fan-in"
 		sed -i "" "/.*weightInitMethod.*/s//weightInitMethod = 1;/g" architecture.conf
 		octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt
  		#cat architecture.conf
