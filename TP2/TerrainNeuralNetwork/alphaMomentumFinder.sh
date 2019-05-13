af736150f8a02a8f107a04ef38ff1dbaaad541fb
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-13 01:06:44 -0300

    alphaMomentum script updated

--- TP2/TerrainNeuralNetwork/alphaMomentumFinder.sh
+++ TP2/TerrainNeuralNetwork/alphaMomentumFinder.sh
@@ -11,7 +11,7 @@ for alphaMomentum in $(seq 0.1 0.1 0.9)
   do
 		echo "Testing with $alphaMomentum nodes in the hidden layer"
 		sleep 1
-		sed -i "" "/.*alphaMomentum.*/s//global alphaMomentum = $alphaMomentum;/g" architecture.conf
+		sed -i "" "/.*alphaMomentum.*/s//rand(\"seed\", $RANDOM);/g" architecture.conf
 		octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt
  		#cat architecture.conf
  done
