f8a4de338fb122ac539fb631829873133eec269f
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-13 00:07:07 -0300

    script updated

--- TP2/TerrainNeuralNetwork/alphaMomentumLowerDeltaFinder.sh
+++ TP2/TerrainNeuralNetwork/alphaMomentumLowerDeltaFinder.sh
@@ -7,13 +7,13 @@ trap '
 
 cp architecture.conf architecture.conf.bak
 rm configurationResults.txt > /dev/null 2>&1
-for alphaMomentum in $(seq 0.2 0.01 0.3)
+for alphaMomentum in $(seq 0.21 0.01 0.3)
   do
 		echo "Testing with $alphaMomentum nodes in the hidden layer"
 		sleep 1
 		sed -i "" "/.*alphaMomentum.*/s//global alphaMomentum = $alphaMomentum;/g" architecture.conf
-		#octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt
- 		cat architecture.conf
+		octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt
+ 		#cat architecture.conf
  done
 cp architecture.conf.bak architecture.conf
 rm architecture.conf.bak
