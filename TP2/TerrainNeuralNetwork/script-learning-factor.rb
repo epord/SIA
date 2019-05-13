d8c108d3af4f2faa0df46276b7fbf7d6c892aec6
Juan Li Puma <juanlipuma94@gmail.com>
2019-05-12 22:03:46 -0300

    Rename ruby script

--- TP2/TerrainNeuralNetwork/script-learning-factor.rb
+++ TP2/TerrainNeuralNetwork/script-learning-factor.rb
@@ -4,13 +4,11 @@
 `rm configurationResults.txt > /dev/null 2>&1`
 
 file_name = "architecture.conf"
-(0.1..0.9).step(0.1).each do |learningFactor|
+[0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29].each do |learningFactor|
     puts "Testing with learning rate #{learningFactor}"
     sleep 1
     contents = File.read(file_name)
     new_contents = contents.sub(/learningFactor = .+/, "learningFactor = #{learningFactor};")
-    puts new_contents
-    gets
     File.open(file_name, "w") {|file| file.puts new_contents }
     `octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt`
 end
