6bef3955f7096b888528e4e49a82f8840398f225
Juan Li Puma <juanlipuma94@gmail.com>
2019-05-13 01:08:54 -0300

    Change configuration finder script to work on 2 layers

--- TP2/TerrainNeuralNetwork/script-configuration.rb
+++ TP2/TerrainNeuralNetwork/script-configuration.rb
@@ -4,16 +4,14 @@
 `rm configurationResults.txt > /dev/null 2>&1`
 
 file_name = "architecture.conf"
-(2..30).step(8).each do |layer1|
-    (2..30).step(8).each do |layer2|
-        (2..30).step(8).each do |layer3|
-            puts "Testing with [2, #{layer1}, #{layer2}, #{layer3}, 1]"
-            sleep 1
-            contents = File.read(file_name)
-            new_contents = contents.sub(/UnitsQuantity = .+/, "UnitsQuantity = [2, #{layer1}, #{layer2}, #{layer3}, 1];")
-            File.open(file_name, "w") {|file| file.puts new_contents }
-            `octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt`
-        end
+(2..22).step(4).each do |layer1|
+    (2..22).step(4).each do |layer2|
+        puts "Testing with [2, #{layer1}, #{layer2}, 1]"
+        sleep 1
+        contents = File.read(file_name)
+        new_contents = contents.sub(/UnitsQuantity = .+/, "UnitsQuantity = [2, #{layer1}, #{layer2}, 1];")
+        File.open(file_name, "w") {|file| file.puts new_contents }
+        `octave multiLayerPerceptron.m --silent --auto-exit | tee -a configurationResults.txt`
     end
 end
 `cp architecture.conf.bak architecture.conf`
