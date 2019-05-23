60b0871711fa9d91ad0d3394e81815299910ff38
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-22 23:15:40 -0300

    multipliers code commented

--- TP3/src/main/java/ar/edu/itba/sia/Warriors/Archer.java
+++ TP3/src/main/java/ar/edu/itba/sia/Warriors/Archer.java
@@ -1,10 +1,13 @@
 package ar.edu.itba.sia.Warriors;
 
 import ar.edu.itba.sia.Items.*;
+import ar.edu.itba.sia.util.Constants;
+
+import static ar.edu.itba.sia.util.Properties.getDouble;
 
 public class Archer extends Warrior {
-    private static final double ATTACK_MULTIPLIER = 0.9;//should read from properties? TODO
-    private static final double DEFENSE_MULTIPLIER = 0.1;//should read from properties? TODO
+    private static final double ATTACK_MULTIPLIER = 0.9; //getDouble(Constants.ATTACK_MULTIPLIER);
+    private static final double DEFENSE_MULTIPLIER = 0.1; //getDouble(Constants.DEFENSE_MULTIPLIER);
 
     public Archer(Boot boots, Gloves gloves, Platebody platebody, Helmet helmet, Weapon weapon, double height) {
         super(boots, gloves, platebody, helmet, weapon, height);
