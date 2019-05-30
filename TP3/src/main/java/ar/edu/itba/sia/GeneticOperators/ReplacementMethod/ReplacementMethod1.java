3306cb301c2c940d7923ac5ab90b31a009c16b04
Agustin Izaguirre <Agustinizag1@gmail.com>
2019-05-30 11:51:14 -0300

    replacement Method1 implemented

--- TP3/src/main/java/ar/edu/itba/sia/GeneticOperators/ReplacementMethod/ReplacementMethod1.java
+++ TP3/src/main/java/ar/edu/itba/sia/GeneticOperators/ReplacementMethod/ReplacementMethod1.java
@@ -5,9 +5,33 @@ import ar.edu.itba.sia.Warriors.Warrior;
 
 import java.util.List;
 
+import static ar.edu.itba.sia.BestWarriorFinder.generateChildren;
+import static ar.edu.itba.sia.BestWarriorFinder.mutatePopulation;
+
 public class ReplacementMethod1 implements ReplacementMethod {
-//TODO
-   public  List<Warrior> getNetGeneration(List<Warrior> population, int k) {
-       return null;
-   }
+    private Selection selectionMethod;
+    private Mutation mutationMethod;
+    private CrossOver crossOverMethod;
+    private Selection replacementMethod;
+
+    public ReplacementMethod1(Selection selectionMethod, Mutation mutationMethod,
+                              CrossOver crossOverMethod, Selection replacementMethod) {
+        this.selectionMethod    = selectionMethod;
+        this.mutationMethod     = mutationMethod;
+        this.crossOverMethod    = crossOverMethod;
+        this.replacementMethod  = replacementMethod;
+    }
+
+    public List<Warrior> getNetGeneration(List<Warrior> population, int k) {
+        List <Warrior> generators;
+        List <Warrior> nextGeneration;
+        int populationNumber = population.size();
+        // Select K fittest parents
+        generators = selectionMethod.select(population, populationNumber);
+        // Cross them (generate children)
+        nextGeneration = generateChildren(crossOverMethod, generators, populationNumber);
+        // Mutate children
+        nextGeneration = mutatePopulation(mutationMethod, nextGeneration, 0.01);
+        return nextGeneration;
+    }
 }
