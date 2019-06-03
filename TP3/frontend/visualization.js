8ff11b72b0986a40c7ef2f33294f47932d926903
epord <pedro.balaguer@hotmail.com>
2019-06-03 01:49:31 -0300

    Visualization re fachera

--- TP3/frontend/visualization.js
+++ TP3/frontend/visualization.js
@@ -50,6 +50,8 @@ function draw() {
 
     drawGenerationCounter();
     drawPopulationClasses();
+    drawCommonItems();
+    drawMeanItems();
 
     ++currentGeneration;
 
@@ -57,13 +59,13 @@ function draw() {
 const drawGenerationCounter = () => {
     textSize(40);
     fill(0);
-    text(currentGeneration, 20, 50);
+    text(currentGeneration, 80, 50);
 };
 
 const drawPopulationClasses = () => {
+    const generationData = generations[currentGeneration];
     for (let i = 0; i < 10; i++) {
         for (let j = 0; j < 10; j++) {
-            const generationData = generations[currentGeneration];
             const populationID = 10 * j + i;
             const classesCount = generationData.distributionsCount.length;
             let accumulatedPopulation = 0;
@@ -81,6 +83,77 @@ const drawPopulationClasses = () => {
             }
         }
     }
+};
+
+const drawCommonItems = () => {
+    const generationData = generations[currentGeneration];
+    const { commonHelmet, commonPlatebody, commonGloves, commonWeapon, commonBoots } = generationData;
+    textSize(20);
+    textAlign(CENTER)
+    fill('black');
+    text("Most\ncommon", 40, 500)
+    drawCommonItem(100, 460, commonHelmet, 27);
+    drawCommonItem(250, 460, commonPlatebody, 37);
+    drawCommonItem(400, 460, commonGloves, 9);
+    drawCommonItem(550, 460, commonWeapon, 47);
+    drawCommonItem(700, 460, commonBoots, 9);
+
+};
+
+// Max values:
+// helmet -> 27
+// platebody -> 37
+// gloves -> 9
+// weapon -> 47
+// boots -> 9
+const drawCommonItem = (x, y, item, maxValue) => {
+    textSize(16);
+    text("St", x, y);
+    rect(x + 30, y - 10, item[1] * 100 / maxValue, 10);
+    text("Ag", x, y + 20);
+    rect(x + 30, y + 10, item[2] * 100 / maxValue, 10);
+    text("Ex", x, y + 40);
+    rect(x + 30, y + 30, item[3] * 100 / maxValue, 10);
+    text("Re", x, y + 60);
+    rect(x + 30, y + 50, item[4] * 100 / maxValue, 10);
+    text("Hi", x, y + 80);
+    rect(x + 30, y + 70, item[5] * 100 / maxValue, 10);
+};
+
+
+const drawMeanItems = () => {
+    const generationData = generations[currentGeneration];
+    const { meanHelmet, meanPlatebody, meanGloves, meanWeapon, meanBoots } = generationData;
+    textSize(20);
+    textAlign(CENTER)
+    fill('black');
+    text("Mean\nitems", 40, 640)
+    drawMeanItem(100, 600, meanHelmet, 27);
+    drawMeanItem(250, 600, meanPlatebody, 37);
+    drawMeanItem(400, 600, meanGloves, 9);
+    drawMeanItem(550, 600, meanWeapon, 47);
+    drawMeanItem(700, 600, meanBoots, 9);
+};
+
+// Max values:
+// helmet -> 27
+// platebody -> 37
+// gloves -> 9
+// weapon -> 47
+// boots -> 9
+const drawMeanItem = (x, y, item, maxValue) => {
+    textSize(16);
+    fill('black');
+    text("St", x, y);
+    rect(x + 30, y - 10, item[0] * 100 / maxValue, 10);
+    text("Ag", x, y + 20);
+    rect(x + 30, y + 10, item[1] * 100 / maxValue, 10);
+    text("Ex", x, y + 40);
+    rect(x + 30, y + 30, item[2] * 100 / maxValue, 10);
+    text("Re", x, y + 60);
+    rect(x + 30, y + 50, item[3] * 100 / maxValue, 10);
+    text("Hi", x, y + 80);
+    rect(x + 30, y + 70, item[4] * 100 / maxValue, 10);
 };
 
 
